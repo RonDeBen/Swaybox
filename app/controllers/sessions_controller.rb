5bd1b3b1999e7be706b702e2cf1cd1d7175cf21c
Ron <RonDeBen@gmail.com>
2015-05-14 17:39:04 -0500

    fixed the login issues

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,9 +5,8 @@ class SessionsController < ApplicationController
   def create
   	user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      log_in(user)
+      log_in user
       redirect_to user
-      root_url :to => current_user
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'

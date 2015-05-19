ecc105a01fb12a7715dbd5e33f13fb2bc569b45e
Ron <RonDeBen@gmail.com>
2015-05-19 13:33:47 -0500

    fixed users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,22 @@
 class UsersController < ApplicationController
   before_filter :authorize, only: :index
   include SessionsHelper
+
+  def index
+    @users = User.all
+  end
+
   def show
   	@user = User.find(params[:id])
-  	debugger
+  	#debugger
   end
 
   def new
   	@user = User.new
+  end
+
+  def edit
+    @user = User.find(params[:id])
   end
 
   def create
@@ -18,6 +27,23 @@ class UsersController < ApplicationController
   	else
   		render 'new'
   	end
+  end
+
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        if is_admin?
+          format.html { redirect_to users_path, notice: "#{@user.name} was successfully updated." }
+          format.json { render :show, status: :ok, location: @user }
+        else
+          format.html { redirect_to @user, notice: "#{@user.name} was successfully updated." }
+          format.json { render :show, status: :ok, location: @user }
+        end
+      else
+        format.html { render :edit }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def login

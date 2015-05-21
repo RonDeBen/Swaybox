e5eff6e3c913e1e5b9cb947872f6e16e994c1dc9
Ron <RonDeBen@gmail.com>
2015-05-21 10:43:01 -0500

    jk now I fixed the users issue

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -65,6 +65,6 @@ class UsersController < ApplicationController
     end
 
 	  def user_params
-	      params.require(:user).permit(:name, :email, :password,:password_confirmation)
+	      params.require(:user).permit(:name, :email, :password,:password_confirmation, :admin, [:id])
 	  end
 end

87a6c95ba0aff80d8c810529bd89b5f7fe022b01
Ron <RonDeBen@gmail.com>
2015-06-07 23:53:32 -0500

    fixed completed wide puppets bug

--- app/controllers/shot_breakdowns_controller.rb
+++ app/controllers/shot_breakdowns_controller.rb
@@ -22,13 +22,14 @@ class ShotBreakdownsController < ApplicationController
     @incomplete_props = []
 
     @layers.each do |layer|
-      @complete_wide_puppets << layer.wide_puppets.where(status: WideStatus.find_by(stage: 'Completed').id) unless layer.wide_puppets.where(status: WideStatus.find_by(stage: 'Completed').id.blank?) 
+      @complete_wide_puppets << layer.wide_puppets.where(status: WideStatus.find_by(stage: 'Completed').id) unless layer.wide_puppets.where(status: WideStatus.find_by(stage: 'Completed').id).blank?
       @complete_mc_puppets << layer.mc_puppets.where(status: McStatus.find_by(stage: 'Completed').id) unless layer.mc_puppets.where(status: McStatus.find_by(stage: 'Completed').id).blank?  
       @complete_props << layer.props.where(status: RegStatus.find_by(stage: 'Completed').id) unless layer.props.where(status: RegStatus.find_by(stage: 'Completed').id).blank?  
       @incomplete_wide_puppets << layer.wide_puppets.where.not(status: WideStatus.find_by(stage: 'Completed').id) unless layer.wide_puppets.where.not(status: WideStatus.find_by(stage: 'Completed').id).blank?  
       @incomplete_mc_puppets << layer.mc_puppets.where.not(status: McStatus.find_by(stage: 'Completed').id) unless layer.mc_puppets.where.not(status: McStatus.find_by(stage: 'Completed').id).blank?  
       @incomplete_props << layer.props.where.not(status: RegStatus.find_by(stage: 'Completed').id) unless layer.props.where.not(status: RegStatus.find_by(stage: 'Completed').id).blank?  
     end
+
     @complete_wide_puppets = @complete_wide_puppets.flatten
     @complete_mc_puppets = @complete_mc_puppets.flatten 
     @complete_props = @complete_props.flatten

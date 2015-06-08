420a23cfe3fbc03cbfa4308be36a8c50c199979f
Ron <RonDeBen@gmail.com>
2015-06-07 21:11:30 -0500

    fixed the shot complete/incomplete tables

--- app/controllers/shot_breakdowns_controller.rb
+++ app/controllers/shot_breakdowns_controller.rb
@@ -13,12 +13,14 @@ class ShotBreakdownsController < ApplicationController
   def show
     @shot_breakdown = ShotBreakdown.find(params[:id])
     @layers = Layer.where(shot_breakdown_id: @shot_breakdown.id)
+
     @complete_wide_puppets = []
     @complete_mc_puppets = [] 
     @complete_props = []
     @incomplete_wide_puppets = []
     @incomplete_mc_puppets = [] 
     @incomplete_props = []
+    
     @layers.each do |layer|
       @complete_wide_puppets << layer.wide_puppets.where(id: WideStatus.find_by(stage: 'Completed').id) unless layer.wide_puppets.where(id: WideStatus.find_by(stage: 'Completed').id.blank?) 
       @complete_mc_puppets << layer.mc_puppets.where(id: McStatus.find_by(stage: 'Completed').id) unless layer.mc_puppets.where(id: McStatus.find_by(stage: 'Completed').id).blank?  
